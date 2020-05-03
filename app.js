8a8afa9853a7789bc3014c2e6b3d524d3b2502c7
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-05-03 13:32:54 -0400

    proofread code

--- app.js
+++ app.js
@@ -3,3 +3,43 @@ const mole = document.querySelectorAll('.mole');
 const timeLeft = document.querySelector('#time-left');
 let score = document.querySelector('#score');
 
+let result = 0
+let currentTime = timeLeft.textContent
+
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove('mole')
+    })
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+
+    hitPosition = randomPosition.id
+}
+
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        if(id.id === hitPosition) {
+            result = result + 1
+            score.textContent = result
+        }
+    })
+})
+
+function moveMole () {
+    let timerId = null
+    timerId = setInterval(randomSquare, 1000)
+}
+
+moveMole()
+
+function countDown () {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime === 0) {
+        clearInterval(timerId)
+        alert(`GAME OVER! You\'ve hit ${result} zombies!`)
+    }
+}
+
+let timerId = setInterval(countDown, 1000)
